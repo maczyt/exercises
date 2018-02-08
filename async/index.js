a889e0144c21097116fc43c80c40afc41d81cfc2
maczyt <369280416@qq.com>
2018-02-08 21:46:31 +0800

    finish async all exercise

--- async/index.js
+++ async/index.js
@@ -4,10 +4,33 @@ const chainAsync = fns => {
   next();
 };
 
+const promiseify = func => (...args) =>
+  new Promise((resolve, reject) => {
+    func(...args, (err, result) => (err ? reject(err) : resolve(result)));
+  });
+
+const parallel = fns => cb => {
+  Promise.all(fns.map(fn => promiseify(fn))).then(p => {
+    Promise.all(p.map(f => f())).then(result => {
+      cb(null, result);
+    });
+  });
+};
+
+const race = fns => cb => {
+  Promise.all(fns.map(fn => promiseify(fn))).then(p => {
+    Promise.race(p.map(f => f())).then(result => {
+      cb(null, result);
+    });
+  });
+};
+
 module.exports = {
   sequence: function(fns) {
     return function(fn) {
       chainAsync(fns.concat(fn));
     };
-  }
+  },
+  parallel,
+  race
 };
